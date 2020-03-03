7b7104305db7e663b20facff453364bc54a4d036
Maayan-Meow <maayanyizz@gmail.com>
2020-03-03 20:53:32 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { hushRouster as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/">HomePage</Link>
+        <Link to="/page2">2</Link>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => {
+              return <h1>HomePage</h1>;
+            }}
+          ></Route>
+          <Route
+            exact
+            path="/page2"
+            component={() => {
+              return <h1>Page2</h1>;
+            }}
+          ></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
