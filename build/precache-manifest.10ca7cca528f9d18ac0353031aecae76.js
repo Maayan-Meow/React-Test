5ad9b59d45a9009d2bf16c2bce47468094e99566
Maayan-Meow <maayanyizz@gmail.com>
2020-03-03 21:11:11 +0200

    gbfgj

---
+++ build/precache-manifest.10ca7cca528f9d18ac0353031aecae76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328210a3cf9c9d2549c26905d1fe7304",
+    "url": "/nameOfReposetory/index.html"
+  },
+  {
+    "revision": "9e2cafe8372e6809d48d",
+    "url": "/nameOfReposetory/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "de388d8e299fac06d666",
+    "url": "/nameOfReposetory/static/js/2.eb7b57f1.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/nameOfReposetory/static/js/2.eb7b57f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2cafe8372e6809d48d",
+    "url": "/nameOfReposetory/static/js/main.1466b91e.chunk.js"
+  },
+  {
+    "revision": "29be56d28a2fa0593c92",
+    "url": "/nameOfReposetory/static/js/runtime-main.98806364.js"
+  }
+]);
